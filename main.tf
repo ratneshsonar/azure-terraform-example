c79770a1e157f54e43e174a4efc373c27bd2db6c
Ratnesh Sonar <ratneshsonar2011@gmail.com>
2025-04-17 08:25:32 +0530

    Update main.tf
    Azure details added

--- main.tf
+++ main.tf
@@ -1,112 +1,55 @@
-provider "aws" {
-  region = "us-east-1"
+provider "azurerm" {
+  features {}
 }
 
-######################
-# 1. VPC
-######################
-resource "aws_vpc" "main" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-  tags = {
-    Name = "main-vpc"
-  }
+# Resource Group
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
 }
 
-######################
-# 2. Internet Gateway
-######################
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-  tags = {
-    Name = "main-igw"
-  }
+# Virtual Network
+resource "azurerm_virtual_network" "main" {
+  name                = var.vnet_name
+  address_space       = var.address_space
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
 }
 
-######################
-# 3. Public Subnet
-######################
-resource "aws_subnet" "public" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
-  availability_zone       = "us-east-1a"
-  tags = {
-    Name = "public-subnet"
-  }
+# Subnets
+resource "azurerm_subnet" "subnets" {
+  for_each = var.subnets
+
+  name                 = each.key
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = [each.value.cidr]
 }
 
-######################
-# 4. Private Subnet
-######################
-resource "aws_subnet" "private" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1a"
-  tags = {
-    Name = "private-subnet"
-  }
+# Network Security Group
+resource "azurerm_network_security_group" "main" {
+  name                = "nsg-main"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
 }
 
-######################
-# 5. Elastic IP for NAT Gateway
-######################
-resource "aws_eip" "nat_eip" {
-  domain = "vpc"
+# Allow SSH Inbound Rule
+resource "azurerm_network_security_rule" "ssh" {
+  name                        = "allow-ssh"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.main.name
+  network_security_group_name = azurerm_network_security_group.main.name
 }
 
-######################
-# 6. NAT Gateway
-######################
-resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.public.id
-  tags = {
-    Name = "main-nat-gateway"
-  }
-
-  depends_on = [aws_internet_gateway.igw]
-}
-
-######################
-# 7. Public Route Table
-######################
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
-  tags = {
-    Name = "public-rt"
-  }
-}
-
-resource "aws_route_table_association" "public_assoc" {
-  subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.public.id
-}
-
-######################
-# 8. Private Route Table
-######################
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat.id
-  }
-
-  tags = {
-    Name = "private-rt"
-  }
-}
-
-resource "aws_route_table_association" "private_assoc" {
-  subnet_id      = aws_subnet.private.id
-  route_table_id = aws_route_table.private.id
+# Associate NSG with Public Subnet
+resource "azurerm_subnet_network_security_group_association" "public" {
+  subnet_id                 = azurerm_subnet.subnets["subnet-public"].id
+  network_security_group_id = azurerm_network_security_group.main.id
 }
